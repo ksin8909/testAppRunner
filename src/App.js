b08804a10762828d2f694e9777778e06865a2f0a
unknown <ksin8909@gmail.com>
2022-08-22 15:08:30 +0800

    0.4

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Test AWS App Runner with Node.js 3
+          Test AWS App Runner with Node.js 4
         </p>
         <a
           className="App-link"
